aece72e83270c12c12de7e834e0ca06430572af8
Brad Durrow <brad@nolab.org>
2019-02-18 09:13:22 -0700

    Changes to renew.sh to fix a variety of problems with my environment

--- renew.sh
+++ renew.sh
@@ -22,9 +22,12 @@
 
 # Set strict mode
 set -euo pipefail
+#set -x
 
 # Version
 version='0.0.2'
+
+letsencrypt_live_dir="/etc/letsencrypt/live"
 
 # Exit if not being run as root
 if [ "${EUID:-$(id -u)}" -ne "0" ] ; then
@@ -42,6 +45,9 @@ fi
 # Default is interactive mode unless already set
 interactive="${interactive:-true}"
 
+
+# Print to stderr
+errcho(){ >&2 echo $@; }
 
 # Safely loads config file
 # First parameter is filename, all consequent parameters are assumed to be
@@ -73,20 +79,61 @@ function load_config()
 # for the first/shortest principal in IPA, this can be overwritten using the
 # email environment variable, for example:
 # email="admin@example.com" ./renew.sh
-load_config '/etc/ipa/default.conf' realm
-host="$(hostname)"
+if [[ -n "${IPA_CONFIG_FILE:-}" ]]; then
+  #Skip setup because IPA_CONFIG_FILE is already set
+  cd . #NO-OP, do nothing
+elif [[ -e "/etc/ipa/server.conf" ]]; then
+  IPA_CONFIG_FILE="/etc/ipa/server.conf"
+elif [[ -e "/etc/ipa/default.conf" ]]; then
+  IPA_CONFIG_FILE="/etc/ipa/default.conf"
+else
+  errcho "FATAL: No ipa config found; exiting!!!"
+  exit 1
+fi
+
+load_config ${IPA_CONFIG_FILE} realm domain
+
+#I am not sure if this is the desired logic but
+#sometimes the hosting platform mangles the hostname
+#such that the domain name is provided by the platform
+#instead of my intended hostname
+if [[ -n ${domain} ]]; then
+  host="$(hostname -s).${domain}"
+else
+  host="$(hostname --fqdn)"
+fi
+
+
 # Get kerberos ticket to modify DNS entries
-kinit -k -t /etc/lets-encrypt.keytab "lets-encrypt/${host}"
-domain_args="$(ipa host-show ${host} --raw | grep krbprincipalname | grep 'host/' | sed 's.krbprincipalname: host/.-d .' | sed s/@${realm}//g | sort -r)"
-dns_domain_name="$(echo ${host} | awk -F. '{OFS="."; print $(NF-1), $NF; }')"
+if [[ ! -e /etc/lets-encrypt.keytab ]]; then
+  errcho "FATAL: /etc/lets-encrypt.keytab not found; exiting!!!"
+  exit 1
+fi
+
+if ! kinit -k -t /etc/lets-encrypt.keytab "lets-encrypt/${host}"; then
+  errcho "FATAL: Could not kinit (RC=$?); exiting!!!" 
+fi
+
+#Calculate the domain part of the hostname
+dns_domain_name="${host#*\.}"
+if [[ ${dns_domain_name} != ${domain} ]]; then
+  errcho "WARNING: ${dns_domain_name} != ${domain}; continuing anyway..."
+fi
+
+#Calculate the host name arguments to pass to certbot
+domain_args="$(ipa host-show ${host} --raw | sed --quiet --expression 's/^[[:space:]]*krbprincipalname:[[:space:]]*host\/\(.*\)@'${realm}'[[:space:]]*$/-d \1/p')"
+
+#Calculate additional arguments to pass to certbot
 soa_record="$(dig SOA ${dns_domain_name} + short | grep ^${dns_domain_name}. | grep 'SOA' | awk '{print $6}')"
 hostmaster="${soa_record/\./@}"
 email="${email:-${hostmaster%\.}}"
-letsencrypt_live_dir="/etc/letsencrypt/live"
-letsencrypt_pem_dir="$(find -L ${letsencrypt_live_dir} -newermt @${start_time_epoch} -type f -name 'privkey.pem' -exec dirname {} \;)"
+
+#Probably should do some kind of validataion on
+#the above calculated values
 
 # Apply for a new cert using CertBot with DNS verification
-certbot certonly --manual \
+certbot certonly --quiet \
+                 --manual \
                  --preferred-challenges dns \
                  --manual-public-ip-logging-ok \
                  --manual-auth-hook 'ipa dnsrecord-mod ${CERTBOT_DOMAIN#*.}. _acme-challenge.${CERTBOT_DOMAIN}. --txt-rec=${CERTBOT_VALIDATION}' \
@@ -96,12 +143,24 @@ certbot certonly --manual \
                  --expand \
                  -n
 
-# If the certificate has bee updated since start of this script
-if [ -n "${letsencrypt_pem_dir}" ] ; then
-    # Install the new Key/Cert, root does not need passwords like mere mortals
-    echo '' | ipa-server-certinstall -w -d "${letsencrypt_pem_dir}/fullchain.pem" "${letsencrypt_pem_dir}/privkey.pem" --dirman-password='' --pin=''
+#Search for directory containing updated privkey.pem
+letsencrypt_pem_dir="$(find -L ${letsencrypt_live_dir} -newermt @${start_time_epoch} -type f -name 'privkey.pem' -exec dirname {} \;)"
 
-    # Restart Web and Directory Servers
-    systemctl restart httpd.service
-    systemctl restart "dirsrv@${realm//./-}.service"
+# Exit if no certificate has been updated since start of script execution
+if [[ -z "${letsencrypt_pem_dir}" ]]; then
+  if [[ ${interactive} == true ]]; then
+    errcho "WARNING: Certificates not updated; if you think this was wrong use the following command to import them:"
+    errcho "  ipa-server-certinstall -w -d <PATH/TO/fullchain.pem> <PATH/TO/privkey.pem>"
+  fi
+  exit 0
 fi
+
+# Install the new Key/Cert, root does not need passwords like mere mortals
+echo '' | ipa-server-certinstall -w -d "${letsencrypt_pem_dir}/fullchain.pem" "${letsencrypt_pem_dir}/privkey.pem" --dirman-password='' --pin=''
+
+echo "Certificates updated; restarting services"
+
+# Restart Web and Directory Servers
+# systemctl restart sudo systemctl restart pki-tomcatd@pki-tomcat
+# systemctl restart "dirsrv@${realm//./-}.service"
+ipactl restart
